9998cb22343c7a22280cb123d4599e781f329ce6
Ameilya Monson <ameilyamonson@gmail.com>
2022-10-11 20:33:54 +0000

    got API to return a response

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ document.getElementById("searchMealSubmit").addEventListener("click", function(e
     return;
   console.log(value);
   const url = "www.themealdb.com/api/json/v1/1/search.php?s=" + value;
+  // let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
   fetch(url)
     .then(function(response) {
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
       return response.json();
-    }).then(function(json) {	
+    }).then(function(json) {
       let results = "";
       results += '<h2>Recipe for ' + json.strMeal + "</h2>";
       results += '<p>Category: ' + json.strCategory + '</p>';
-      results += '<p>Instructions: ' + json.str.Instructions + '</p>';
+      results += '<p>Instructions: ' + json.strInstructions + '</p>';
       results += '<img src="' + json.strMealThumb + '"/>';
       results += "<br/>";
       document.getElementById("searchMealResults").innerHTML = results;
