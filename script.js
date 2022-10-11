206406f4272976164522c618932134e4286bbe2e
Ameilya Monson <ameilyamonson@gmail.com>
2022-10-11 20:39:25 +0000

    fixed url to include https

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("searchMealSubmit").addEventListener("click", function(e
   if (value === "")
     return;
   console.log(value);
-  const url = "www.themealdb.com/api/json/v1/1/search.php?s=" + value;
+  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + value;
   // let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
   fetch(url)
     .then(function(response) {
@@ -16,13 +16,11 @@ document.getElementById("searchMealSubmit").addEventListener("click", function(e
       }
       return response.json();
     }).then(function(json) {
+      console.log(json);
       let results = "";
       results += '<h2>Recipe for ' + json.strMeal + "</h2>";
       results += '<p>Category: ' + json.strCategory + '</p>';
-<<<<<<< HEAD
       results += '<p>Instructions: ' + json.strInstructions + '</p>';
-=======
->>>>>>> c5a8b03e0fd5e85c2f920c8a7e4785005402b92b
       results += '<img src="' + json.strMealThumb + '"/>';
       results += "<br/>";
       document.getElementById("searchMealResults").innerHTML = results;
